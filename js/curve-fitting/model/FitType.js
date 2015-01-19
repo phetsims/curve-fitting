d8267df4d79d52aae8089e6d8fc9387a9b9d1724
MLearner05 <andrey.zelenkov@mlearner.com>
2015-01-19 20:41:47 +0300

    added enum types

---
+++ js/curve-fitting/model/FitType.js
@@ -0,0 +1,16 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ * Fit types in the 'Curve Fitting' simulation.
+ *
+ * @author Andrey Zelenkov (Mlearner)
+ */
+
+define( function() {
+  'use strict';
+
+  return Object.freeze( {
+    BEST: 'linear',
+    ADJUSTABLE: 'adjustable'
+  } );
+} );
