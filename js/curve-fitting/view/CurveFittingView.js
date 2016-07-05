eb709ea0b32b8076b2bfbad026e22bbd1ef86fdb
veillette <martin.veillette@gmail.com>
2016-07-04 22:01:24 -0400

    layout the graph using the left and right coordinates of the two panels instead of their width (more robust)

--- js/curve-fitting/view/CurveFittingView.js
+++ js/curve-fitting/view/CurveFittingView.js
@@ -23,6 +23,7 @@ define( function( require ) {
   // constants
   var PADDING_LEFT_RIGHT = 10;
   var PADDING_TOP_BOTTOM = 25;
+  var GRAPH_PADDING_LEFT_RIGHT = 15;
   var SIM_BOUNDS = CurveFittingConstants.SIM_BOUNDS;
   var GRAPH_MODEL_BOUNDS = CurveFittingConstants.GRAPH_MODEL_BOUNDS;
 
@@ -39,16 +40,6 @@ define( function( require ) {
     // create control menu node
     var controlMenuNode = new ControlMenuNode( curveFittingModel );
 
-    // create a model view transform
-    var graphAreaWidth = SIM_BOUNDS.width - deviationsPanel.width - controlMenuNode.width - 50;
-    var graphCenterX = graphAreaWidth / 2 + deviationsPanel.width + 25;
-    var graphCenterY = SIM_BOUNDS.centerY;
-    var scale = graphAreaWidth / GRAPH_MODEL_BOUNDS.width;
-    var modelViewTransform = ModelViewTransform2.createSinglePointScaleInvertedYMapping( new Vector2( 0, 0 ), new Vector2( graphCenterX, graphCenterY ), scale );
-
-    // create bucket and graph area node
-    var bucketAndGraphAreaNode = new BucketAndGraphAreaNode( curveFittingModel, modelViewTransform );
-
     // create reset all button
     var resetAllButton = new ResetAllButton( {
       listener: function() {
@@ -56,18 +47,30 @@ define( function( require ) {
       }
     } );
 
+    // layout the nodes
+    deviationsPanel.left = PADDING_LEFT_RIGHT;
+    deviationsPanel.top = PADDING_TOP_BOTTOM;
+    controlMenuNode.right = SIM_BOUNDS.width - PADDING_LEFT_RIGHT;
+    controlMenuNode.top = deviationsPanel.top;
+    resetAllButton.scale( 0.75 );
+    resetAllButton.right = controlMenuNode.right;
+    resetAllButton.bottom = SIM_BOUNDS.height - PADDING_TOP_BOTTOM;
+
+    // create a model view transform
+    var graphAreaWidth = controlMenuNode.left - deviationsPanel.right - GRAPH_PADDING_LEFT_RIGHT * 2;
+    var graphCenterX = 0.5 * (controlMenuNode.left + deviationsPanel.right);
+    var graphCenterY = SIM_BOUNDS.centerY;
+    var scale = graphAreaWidth / GRAPH_MODEL_BOUNDS.width;
+    var modelViewTransform = ModelViewTransform2.createSinglePointScaleInvertedYMapping( new Vector2( 0, 0 ), new Vector2( graphCenterX, graphCenterY ), scale );
+
+    // create bucket and graph area node
+    var bucketAndGraphAreaNode = new BucketAndGraphAreaNode( curveFittingModel, modelViewTransform );
+
+    // add the children to the scene graph
     this.addChild( deviationsPanel );
     this.addChild( controlMenuNode );
     this.addChild( bucketAndGraphAreaNode );
     this.addChild( resetAllButton );
-
-    deviationsPanel.left = PADDING_LEFT_RIGHT;
-    deviationsPanel.top = PADDING_TOP_BOTTOM;
-    controlMenuNode.right = SIM_BOUNDS.width - PADDING_LEFT_RIGHT;
-    controlMenuNode.top = PADDING_TOP_BOTTOM;
-    resetAllButton.scale( 0.75 );
-    resetAllButton.right = controlMenuNode.right;
-    resetAllButton.bottom = SIM_BOUNDS.height - PADDING_TOP_BOTTOM;
 
   }
 
