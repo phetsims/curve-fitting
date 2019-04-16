1dfff26110225cc81de3db37acfe06b20c9ed27b
Saurabh Totey <SaurabhTotey@gmail.com>
2019-04-15 21:59:02 -0600

    Finished migration of BarometerR2Node to ES6
    See #117

--- js/curve-fitting/view/BarometerR2Node.js
+++ js/curve-fitting/view/BarometerR2Node.js
@@ -6,14 +6,13 @@
  * @author Andrey Zelenkov (Mlearner)
  * @author Chris Malley (PixelZoom, Inc.)
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
   const Circle = require( 'SCENERY/nodes/Circle' );
   const curveFitting = require( 'CURVE_FITTING/curveFitting' );
   const CurveFittingConstants = require( 'CURVE_FITTING/curve-fitting/CurveFittingConstants' );
-  const inherit = require( 'PHET_CORE/inherit' );
   const Line = require( 'SCENERY/nodes/Line' );
   const Node = require( 'SCENERY/nodes/Node' );
   const PhetFont = require( 'SCENERY_PHET/PhetFont' );
@@ -29,51 +28,47 @@ define( function( require ) {
   };
   const TICK_FONT = new PhetFont( 12 );
 
-  /**
-   * @param {Property.<number>} rSquaredProperty - Property that represents r-deviation.
-   * @param {Property.<boolean>} curveVisibleProperty
-   * @param {Object} [options] for graph node.
-   * @constructor
-   */
-  function BarometerR2Node( rSquaredProperty, curveVisibleProperty,  options ) {
+  class BarometerR2Node extends Node {
 
-    // value shown on the barometer
-    const valueRectNode = new Rectangle( 0, 0, CurveFittingConstants.BAROMETER_BAR_WIDTH, 0, {
-      fill: CurveFittingConstants.BLUE_COLOR
-    } );
-    valueRectNode.rotation = Math.PI;
+    /**
+     * @param {Property.<number>} rSquaredProperty - Property that represents r-deviation.
+     * @param {Property.<boolean>} curveVisibleProperty
+     * @param {Object} [options] for graph node.
+     */
+    constructor( rSquaredProperty, curveVisibleProperty,  options ) {
+      // value shown on the barometer
+      const valueRectNode = new Rectangle( 0, 0, CurveFittingConstants.BAROMETER_BAR_WIDTH, 0, {
+        fill: CurveFittingConstants.BLUE_COLOR
+      } );
+      valueRectNode.rotation = Math.PI;
 
-    const axis = new Line( 0, 0, 0, -CurveFittingConstants.BAROMETER_AXIS_HEIGHT, LINE_OPTIONS );
+      const axis = new Line( 0, 0, 0, -CurveFittingConstants.BAROMETER_AXIS_HEIGHT, LINE_OPTIONS );
 
-    Node.call( this, _.extend( {}, options, {
-      children: [ valueRectNode, axis ]
-    } ) );
+      super( _.extend( {}, options, {
+        children: [ valueRectNode, axis ]
+      } ) );
 
-    this.addTicks( [ 0, 0.25, 0.5, 0.75, 1 ] );
+      this.addTicks( [ 0, 0.25, 0.5, 0.75, 1 ] );
 
-    // put a red dot at the origin
-    if ( phet.chipper.queryParameters.dev ) {
-      this.addChild( new Circle( 3, { fill: 'red' } ) );
+      // put a red dot at the origin
+      if ( phet.chipper.queryParameters.dev ) {
+        this.addChild( new Circle( 3, { fill: 'red' } ) );
+      }
+
+      // no need to unlink, present for the lifetime of the sim
+      rSquaredProperty.link( rSquared => {
+        valueRectNode.setRectHeight( ( RANGE.min + rSquared / RANGE.max ) * CurveFittingConstants.BAROMETER_AXIS_HEIGHT );
+      } );
+      curveVisibleProperty.linkAttribute( valueRectNode, 'visible');
     }
 
-    // no need to unlink, present for the lifetime of the sim
-    rSquaredProperty.link( rSquared => {
-      valueRectNode.setRectHeight( ( RANGE.min + rSquared / RANGE.max ) * CurveFittingConstants.BAROMETER_AXIS_HEIGHT );
-    } );
-    curveVisibleProperty.linkAttribute( valueRectNode, 'visible');
-  }
-
-  curveFitting.register( 'BarometerR2Node', BarometerR2Node );
-
-  return inherit( Node, BarometerR2Node, {
-
-    //TODO #120 very similar to BarometerX2Node
     /**
      * Adds a tick.
+     * TODO #120 very similar to BarometerX2Node
      * @param {number} value
      * @private
      */
-    addTick: function( value ) {
+    addTick( value ) {
 
       //TODO #120 document this better, it's not clear why this is needed, duplicated in BarometerX2Node
       // expression "0.5 + ( CurveFittingConstants.BAROMETER_AXIS_HEIGHT - 1 )" need to prevent bad graph view in corners
@@ -90,19 +85,24 @@ define( function( require ) {
         centerY: line.centerY
       } );
       this.addChild( label );
-    },
+    }
 
-    //TODO #120 entirely duplicated in BarometerX2Node
     /**
      * Adds multiple ticks.
+     * TODO #120 entirely duplicated in BarometerX2Node
      * @param {number[]} values
      * @private
      */
-    addTicks: function( values ) {
+    addTicks( values ) {
       const self = this;
       values.forEach( function( value ) {
         self.addTick( value );
       } );
     }
-  } );
+
+  }
+
+  curveFitting.register( 'BarometerR2Node', BarometerR2Node );
+
+  return BarometerR2Node;
 } );
