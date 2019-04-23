79e927c22e943ddf3c0b8ee1925d649c44a06f0f
Saurabh Totey <SaurabhTotey@gmail.com>
2019-04-23 07:30:29 -0600

    Finished migration of CoefficientSliderNode to ES6
    See #117

--- js/curve-fitting/view/CoefficientSliderNode.js
+++ js/curve-fitting/view/CoefficientSliderNode.js
@@ -5,14 +5,13 @@
  *
  * @author Andrey Zelenkov (Mlearner)
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
   const curveFitting = require( 'CURVE_FITTING/curveFitting' );
   const CurveFittingConstants = require( 'CURVE_FITTING/curve-fitting/CurveFittingConstants' );
   const Dimension2 = require( 'DOT/Dimension2' );
-  const inherit = require( 'PHET_CORE/inherit' );
   const PhetFont = require( 'SCENERY_PHET/PhetFont' );
   const Text = require( 'SCENERY/nodes/Text' );
   const VBox = require( 'SCENERY/nodes/VBox' );
@@ -27,16 +26,16 @@ define( function( require ) {
     fill: CurveFittingConstants.BLUE_COLOR
   };
 
-  /**
-   * @param {Property.<number>} property parameter to track.
-   * @param {Range} range - Possible range for property.
-   * @param {string} label - Label for slider.
-   * @param {Object} [options] for slider node.
-   * @constructor
-   */
-  function CoefficientSliderNode( property, range, label, options ) {
+  class CoefficientSliderNode extends VBox {
 
-    options = _.extend( {
+    /**
+     * @param {Property.<number>} property parameter to track.
+     * @param {Range} range - Possible range for property.
+     * @param {string} label - Label for slider.
+     * @param {Object} [options] for slider node.
+     */
+    constructor( property, range, label, options ) {
+      options = _.extend( {
         trackFill: 'black',
         trackSize: new Dimension2( 120, 1 ),
         thumbSize: new Dimension2( 15, 24 ),
@@ -44,20 +43,22 @@ define( function( require ) {
         thumbTouchAreaYDilation: 1   // small such that touch areas of adjacent sliders don't overlap. It is YDilation since we rotate by 90
       }, options );
 
-    const sliderNode = new VSlider( property, range, options );
+      const sliderNode = new VSlider( property, range, options );
 
-    // add central tick
-    sliderNode.addMinorTick( 0, '' );
+      // add central tick
+      sliderNode.addMinorTick( 0, '' );
 
-    VBox.call( this, _.extend( {
-      children: [
-        sliderNode,
-        new Text( label, LABEL_OPTIONS )
-      ]
-    }, options ) );
+      super( _.extend( {
+        children: [
+          sliderNode,
+          new Text( label, LABEL_OPTIONS )
+        ]
+      }, options ) );
+    }
+
   }
 
   curveFitting.register( 'CoefficientSliderNode', CoefficientSliderNode );
 
-  return inherit( VBox, CoefficientSliderNode );
+  return CoefficientSliderNode;
 } );
