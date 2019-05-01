d9846f65a7aaa0ca68bf964beecfff9cd95a6157
Saurabh Totey <SaurabhTotey@gmail.com>
2019-05-01 10:58:48 -0600

    Migrated ControlPanel to ES6
    See #117

--- js/curve-fitting/view/ControlPanels.js
+++ js/curve-fitting/view/ControlPanels.js
@@ -14,7 +14,6 @@ define( function( require ) {
   const CurveFittingConstants = require( 'CURVE_FITTING/curve-fitting/CurveFittingConstants' );
   const CurveOrderPanel = require( 'CURVE_FITTING/curve-fitting/view/CurveOrderPanel' );
   const FitPanel = require( 'CURVE_FITTING/curve-fitting/view/FitPanel' );
-  const inherit = require( 'PHET_CORE/inherit' );
   const VBox = require( 'SCENERY/nodes/VBox' );
   const ViewOptionsPanel = require( 'CURVE_FITTING/curve-fitting/view/ViewOptionsPanel' );
 
@@ -28,45 +27,47 @@ define( function( require ) {
     minWidth: CurveFittingConstants.PANEL_MIN_WIDTH
   };
 
-  /**
-   * @param {Property.<number>[]} sliderPropertyArray
-   * @param {Property.<number>} orderProperty
-   * @param {Property.<string>} fitProperty
-   * @param {Property.<boolean>} curveVisibleProperty
-   * @param {Property.<boolean>} residualsVisibleProperty
-   * @param {Property.<boolean>} valuesProperty
-   * @param {Object} [options]
-   * @constructor
-   */
-  function ControlPanels( sliderPropertyArray, orderProperty, fitProperty, curveVisibleProperty, residualsVisibleProperty, valuesProperty, options ) {
+  class ControlPanels extends VBox {
 
-    options = _.extend( {
-      align: 'left',
-      spacing: 10
-    }, options );
+    /**
+     * @param {Property.<number>[]} sliderPropertyArray
+     * @param {Property.<number>} orderProperty
+     * @param {Property.<string>} fitProperty
+     * @param {Property.<boolean>} curveVisibleProperty
+     * @param {Property.<boolean>} residualsVisibleProperty
+     * @param {Property.<boolean>} valuesProperty
+     * @param {Object} [options]
+     */
+    constructor( sliderPropertyArray, orderProperty, fitProperty, curveVisibleProperty, residualsVisibleProperty, valuesProperty, options ) {
+      options = _.extend( {
+        align: 'left',
+        spacing: 10
+      }, options );
 
-    // view options
-    const viewOptionsPanel = new ViewOptionsPanel( curveVisibleProperty, residualsVisibleProperty, valuesProperty, PANEL_OPTIONS );
+      // view options
+      const viewOptionsPanel = new ViewOptionsPanel( curveVisibleProperty, residualsVisibleProperty, valuesProperty, PANEL_OPTIONS );
 
-    // order of curve
-    const orderPanel = new CurveOrderPanel( orderProperty, PANEL_OPTIONS );
+      // order of curve
+      const orderPanel = new CurveOrderPanel( orderProperty, PANEL_OPTIONS );
 
-    // fit type
-    const fitPanel = new FitPanel( sliderPropertyArray, fitProperty, orderProperty, PANEL_OPTIONS );
+      // fit type
+      const fitPanel = new FitPanel( sliderPropertyArray, fitProperty, orderProperty, PANEL_OPTIONS );
 
-    assert && assert( !options.children, 'decoration not supported' );
-    options.children = [ viewOptionsPanel, orderPanel, fitPanel ];
+      assert && assert( !options.children, 'decoration not supported' );
+      options.children = [ viewOptionsPanel, orderPanel, fitPanel ];
 
-    VBox.call( this, options );
+      super( options );
 
-    // hide panels when curve is not visible
-    curveVisibleProperty.link( curveVisible => {
-      orderPanel.visible = curveVisible;
-      fitPanel.visible = curveVisible;
-    } );
+      // hide panels when curve is not visible
+      curveVisibleProperty.link( curveVisible => {
+        orderPanel.visible = curveVisible;
+        fitPanel.visible = curveVisible;
+      } );
+    }
+
   }
 
   curveFitting.register( 'ControlPanels', ControlPanels );
 
-  return inherit( VBox, ControlPanels );
+  return ControlPanels;
 } );
