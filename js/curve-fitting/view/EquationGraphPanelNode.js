38bc47c564cf9e6e5c7b61ff984004b809db8452
Saurabh Totey <SaurabhTotey@gmail.com>
2019-05-28 11:28:33 -0600

    Finished migration of EquationGraphPanelNode to ES6
    See #117

--- js/curve-fitting/view/EquationGraphPanelNode.js
+++ js/curve-fitting/view/EquationGraphPanelNode.js
@@ -7,7 +7,7 @@
  *
  * @author Andrey Zelenkov (Mlearner)
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
@@ -15,7 +15,6 @@ define( function( require ) {
   const CurveFittingConstants = require( 'CURVE_FITTING/curve-fitting/CurveFittingConstants' );
   const ExpandCollapseButton = require( 'SUN/ExpandCollapseButton' );
   const HBox = require( 'SCENERY/nodes/HBox' );
-  const inherit = require( 'PHET_CORE/inherit' );
   const MathSymbols = require( 'SCENERY_PHET/MathSymbols' );
   const Panel = require( 'SUN/Panel' );
   const PhetFont = require( 'SCENERY_PHET/PhetFont' );
@@ -49,149 +48,147 @@ define( function( require ) {
   //  max number precision decimal places for ascending order of coefficient of polynomials
   const MAX_DECIMALS = [ 1, 2, 3, 3 ];
 
-  /**
-   * @param {Function} getCoefficientArray - returns an array of coefficient of the polynomial curve sorted in ascending order {<number>[]}
-   * @param {Emitter} updateCurveEmitter
-   * @param {Property.<number>} orderProperty - order of the polynomial:(1,2,3)
-   * @param {Property.<boolean>} equationPanelExpandedProperty
-   * @param {Property.<boolean>} curveVisibleProperty
-   * @param {Object} [options] for slider node.
-   * @constructor
-   */
-  function EquationGraphPanelNode( getCoefficientArray,
-                                   updateCurveEmitter,
-                                   orderProperty,
-                                   equationPanelExpandedProperty,
-                                   curveVisibleProperty,
-                                   options ) {
-
-    //  visible text node when panel is not expanded
-    const titleNode = new Text( equationString, TEXT_OPTIONS );
-
-    //  visible node when panel is expanded
-    const boxNode = new HBox( { align: 'bottom' } );
-
-    // create expand button
-    const expandCollapseButton = new ExpandCollapseButton( equationPanelExpandedProperty, {
-      sideLength: BUTTON_LENGTH
-    } );
-    expandCollapseButton.touchArea = expandCollapseButton.localBounds.dilated( BUTTON_LENGTH / 3 );
-    expandCollapseButton.mouseArea = expandCollapseButton.localBounds.dilated( BUTTON_LENGTH / 3 );
-
-    const content = new HBox( {
-      spacing: 5,
-      children: [ expandCollapseButton, titleNode ]
-    } );
-
-    Panel.call( this, content, _.extend( PANEL_OPTIONS, options ) );
-
-    // convenience array, strings are sorted in ascending order of coefficient of polynomials
-    const symbolStrings = [ symbolDString, symbolCString, symbolBString, symbolAString ];
-
-    // text nodes that contain the numerical value of the polynomial coefficients
-    // strings are place holders that will be updated by numerical value
-    const textNodes = symbolStrings.map( symbolString => new Text( symbolString, PARAMETER_TEXT_OPTIONS ) );
-
-    // blockNode stores all elements of the right hand side of the equation
-    const blockNodes = textNodes.map( ( textNode, index ) => this.createPolynomialHBox( textNode, index ) );
-
-    // create the left hand side of equation ( with equal sign)
-    const yNode = new Text( symbolYString + ' =', TEXT_OPTIONS );
-
-    //  update the relevant blocks on the right hand side of equation.
-    orderProperty.link( function( order ) {
-      // only the relevant orders are shown
-      boxNode.children = [ yNode ].concat( blockNodes.slice( 0, order + 1 ).reverse() );
-    } );
-
-    // toggle the content of the panel, based on the expansion status
-    equationPanelExpandedProperty.link( function( isEquationPanelExpanded ) {
-      if ( isEquationPanelExpanded ) {
-        content.children = [ expandCollapseButton, boxNode ];
-      }
-      else {
-        content.children = [ expandCollapseButton, titleNode ];
-      }
-    } );
+  class EquationGraphPanelNode extends Panel {
 
     /**
-     * Function that returns (for numbers smaller than ten) a number (as a string)  with a fixed number of decimal places
-     * whereas for numbers larger than ten, the number/string is returned a fixed number of significant figures
-     *
-     * @param {number} number
-     * @param {number} maxDecimalPlaces
-     * @returns {Object}
+     * @param {Function} getCoefficientArray - returns an array of coefficient of the polynomial curve sorted in ascending order {<number>[]}
+     * @param {Emitter} updateCurveEmitter
+     * @param {Property.<number>} orderProperty - order of the polynomial:(1,2,3)
+     * @param {Property.<boolean>} equationPanelExpandedProperty
+     * @param {Property.<boolean>} curveVisibleProperty
+     * @param {Object} [options] for slider node.
      */
-    const roundNumber = ( number, maxDecimalPlaces ) => {
+    constructor( getCoefficientArray,
+                                     updateCurveEmitter,
+                                     orderProperty,
+                                     equationPanelExpandedProperty,
+                                     curveVisibleProperty,
+                                     options ) {
 
-      // eg. if maxDecimalPlaces = 3
-      // 9999.11 -> 9999  (number larger than 10^3) are rounded to unity
-      // 999.111 -> 999.1
-      // 99.1111 -> 99.11
-      // 9.11111 -> 9.111
-      // 1.11111 -> 1.111
-      // 0.11111 -> 0.111
-      // 0.01111 -> 0.011
-      // 0.00111 -> 0.001
-      // 0.00011 -> 0.000
+      //  visible text node when panel is not expanded
+      const titleNode = new Text( equationString, TEXT_OPTIONS );
 
-      // number = mantissa times 10^(exponent) where the mantissa is between 1 and 10 (or -1 to -10)
-      const exponent = Math.floor( Util.log10( Math.abs( number ) ) );
+      //  visible node when panel is expanded
+      const boxNode = new HBox( { align: 'bottom' } );
 
-      let decimalPlaces;
-      if ( exponent >= maxDecimalPlaces ) {
-        decimalPlaces = 0;
-      }
-      else if ( exponent > 0 ) {
-        decimalPlaces = maxDecimalPlaces - exponent;
-      }
-      else {
-        decimalPlaces = maxDecimalPlaces;
-      }
-      const roundedNumber = Util.toFixedNumber( number, decimalPlaces );
-      const numberToString = Util.toFixed( number, decimalPlaces );
-      const signToString = (roundedNumber >= 0) ? MathSymbols.PLUS : MathSymbols.MINUS; // N.B.
-      const absoluteNumberToString = Util.toFixed( Math.abs( number ), decimalPlaces ); // N.B.
-      const isStringZero = (numberToString === Util.toFixed( 0, decimalPlaces ));
-
-      return {
-        numberToString: numberToString, // {string}
-        signToString: signToString, // {string}
-        absoluteNumberToString: absoluteNumberToString, // {string}
-        isStringZero: isStringZero  // {boolean}
-      };
-    };
-
-    /**
-     * update the numerical coefficient of a node
-     * @param {number} order
-     * @param {number} maxDecimalPlaces
-     * @param {Text} textNode
-     */
-    const updateCoefficient = ( order, maxDecimalPlaces, textNode ) => {
-      const numberInfo = roundNumber( getCoefficientArray()[ order ], maxDecimalPlaces );
-      textNode.setText( numberInfo.signToString + numberInfo.absoluteNumberToString );
-    };
-
-    /**
-     * update all the coefficients
-     */
-    const updateCoefficients = () => {
-      textNodes.forEach( ( textNode, index ) => {
-        updateCoefficient( index, MAX_DECIMALS[ index ], textNode );
+      // create expand button
+      const expandCollapseButton = new ExpandCollapseButton( equationPanelExpandedProperty, {
+        sideLength: BUTTON_LENGTH
       } );
-    };
+      expandCollapseButton.touchArea = expandCollapseButton.localBounds.dilated( BUTTON_LENGTH / 3 );
+      expandCollapseButton.mouseArea = expandCollapseButton.localBounds.dilated( BUTTON_LENGTH / 3 );
 
-    // add observer, present of the lifetime of the simulation
-    curveVisibleProperty.linkAttribute( this, 'visible' );
-    curveVisibleProperty.link( updateCoefficients );
-    equationPanelExpandedProperty.link( updateCoefficients );
-    updateCurveEmitter.addListener( updateCoefficients );
-  }
+      const content = new HBox( {
+        spacing: 5,
+        children: [ expandCollapseButton, titleNode ]
+      } );
 
-  curveFitting.register( 'EquationGraphPanelNode', EquationGraphPanelNode );
+      super( content, _.extend( PANEL_OPTIONS, options ) );
 
-  return inherit( Panel, EquationGraphPanelNode, {
+      // convenience array, strings are sorted in ascending order of coefficient of polynomials
+      const symbolStrings = [ symbolDString, symbolCString, symbolBString, symbolAString ];
+
+      // text nodes that contain the numerical value of the polynomial coefficients
+      // strings are place holders that will be updated by numerical value
+      const textNodes = symbolStrings.map( symbolString => new Text( symbolString, PARAMETER_TEXT_OPTIONS ) );
+
+      // blockNode stores all elements of the right hand side of the equation
+      const blockNodes = textNodes.map( ( textNode, index ) => this.createPolynomialHBox( textNode, index ) );
+
+      // create the left hand side of equation ( with equal sign)
+      const yNode = new Text( symbolYString + ' =', TEXT_OPTIONS );
+
+      //  update the relevant blocks on the right hand side of equation.
+      orderProperty.link( function( order ) {
+        // only the relevant orders are shown
+        boxNode.children = [ yNode ].concat( blockNodes.slice( 0, order + 1 ).reverse() );
+      } );
+
+      // toggle the content of the panel, based on the expansion status
+      equationPanelExpandedProperty.link( function( isEquationPanelExpanded ) {
+        if ( isEquationPanelExpanded ) {
+          content.children = [ expandCollapseButton, boxNode ];
+        }
+        else {
+          content.children = [ expandCollapseButton, titleNode ];
+        }
+      } );
+
+      /**
+       * Function that returns (for numbers smaller than ten) a number (as a string)  with a fixed number of decimal places
+       * whereas for numbers larger than ten, the number/string is returned a fixed number of significant figures
+       *
+       * @param {number} number
+       * @param {number} maxDecimalPlaces
+       * @returns {Object}
+       */
+      const roundNumber = ( number, maxDecimalPlaces ) => {
+
+        // eg. if maxDecimalPlaces = 3
+        // 9999.11 -> 9999  (number larger than 10^3) are rounded to unity
+        // 999.111 -> 999.1
+        // 99.1111 -> 99.11
+        // 9.11111 -> 9.111
+        // 1.11111 -> 1.111
+        // 0.11111 -> 0.111
+        // 0.01111 -> 0.011
+        // 0.00111 -> 0.001
+        // 0.00011 -> 0.000
+
+        // number = mantissa times 10^(exponent) where the mantissa is between 1 and 10 (or -1 to -10)
+        const exponent = Math.floor( Util.log10( Math.abs( number ) ) );
+
+        let decimalPlaces;
+        if ( exponent >= maxDecimalPlaces ) {
+          decimalPlaces = 0;
+        }
+        else if ( exponent > 0 ) {
+          decimalPlaces = maxDecimalPlaces - exponent;
+        }
+        else {
+          decimalPlaces = maxDecimalPlaces;
+        }
+        const roundedNumber = Util.toFixedNumber( number, decimalPlaces );
+        const numberToString = Util.toFixed( number, decimalPlaces );
+        const signToString = (roundedNumber >= 0) ? MathSymbols.PLUS : MathSymbols.MINUS; // N.B.
+        const absoluteNumberToString = Util.toFixed( Math.abs( number ), decimalPlaces ); // N.B.
+        const isStringZero = (numberToString === Util.toFixed( 0, decimalPlaces ));
+
+        return {
+          numberToString: numberToString, // {string}
+          signToString: signToString, // {string}
+          absoluteNumberToString: absoluteNumberToString, // {string}
+          isStringZero: isStringZero  // {boolean}
+        };
+      };
+
+      /**
+       * update the numerical coefficient of a node
+       * @param {number} order
+       * @param {number} maxDecimalPlaces
+       * @param {Text} textNode
+       */
+      const updateCoefficient = ( order, maxDecimalPlaces, textNode ) => {
+        const numberInfo = roundNumber( getCoefficientArray()[ order ], maxDecimalPlaces );
+        textNode.setText( numberInfo.signToString + numberInfo.absoluteNumberToString );
+      };
+
+      /**
+       * update all the coefficients
+       */
+      const updateCoefficients = () => {
+        textNodes.forEach( ( textNode, index ) => {
+          updateCoefficient( index, MAX_DECIMALS[ index ], textNode );
+        } );
+      };
+
+      // add observer, present of the lifetime of the simulation
+      curveVisibleProperty.linkAttribute( this, 'visible' );
+      curveVisibleProperty.link( updateCoefficients );
+      equationPanelExpandedProperty.link( updateCoefficients );
+      updateCurveEmitter.addListener( updateCoefficients );
+    }
+
     /**
      * returns a hBox containing a numerical coefficient and a polynomial to the power 'order'
      * eg. -4.0 x^3
@@ -200,7 +197,7 @@ define( function( require ) {
      * @returns {HBox}
      * @private
      */
-    createPolynomialHBox: function( coefficientText, order ) {
+    createPolynomialHBox( coefficientText, order ) {
       let polynomialText;
       if ( order === 0 ) {
         polynomialText = new Text( '', TEXT_OPTIONS );
@@ -217,5 +214,10 @@ define( function( require ) {
         children: [ coefficientText, polynomialText ]
       } );
     }
-  } );
+
+  }
+
+  curveFitting.register( 'EquationGraphPanelNode', EquationGraphPanelNode );
+
+  return EquationGraphPanelNode;
 } );
