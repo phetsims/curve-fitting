4301b3d64101d42b8fb5626fc5a05f652ab3dcb6
Saurabh Totey <SaurabhTotey@gmail.com>
2019-07-11 15:57:48 -0600

    Shrunk coefficient slider thumb sizes and increased minor tick length
    See #128

--- js/curve-fitting/view/CoefficientSliderNode.js
+++ js/curve-fitting/view/CoefficientSliderNode.js
@@ -29,9 +29,12 @@ define( require => {
       options = _.extend( {
         trackFill: 'black',
         trackSize: new Dimension2( 120, 1 ),
-        thumbSize: new Dimension2( 15, 23 ),
+        thumbSize: new Dimension2( 8, 20 ),
         minorTickLineWidth: 2,
+        minorTickLength: 12,
         thumbTouchAreaYDilation: 3, // supposed to make touch horizontal areas flush; is YDilation since we rotate by 90; see #72
+        thumbMouseAreaYDilation: 3,
+        thumbMouseAreaXDilation: 10,
         labelOptions: {
           font: new PhetFont( {
             weight: 'bold',
