36f01d4fa9f6c2ec3e06e5b11d2b8522f42f7668
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:01 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/curveFittingStrings.ts
@@ -0,0 +1,46 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import curveFitting from './curveFitting.js';
+
+type StringsType = {
+  'adjustableFit': string,
+  'bestFit': string,
+  'cubic': string,
+  'curve': string,
+  'curve-fitting': {
+    'title': string
+  },
+  'deviations': string,
+  'equation': string,
+  'linear': string,
+  'undefined': string,
+  'fEqualsNumberOfParametersPattern': string,
+  'nEqualsNumberOfDataPointsPattern': string,
+  'deltaEqualsPattern': string,
+  'pointCoordinatesPattern': string,
+  'theReducedChiSquaredStatisticIs': string,
+  'quadratic': string,
+  'residuals': string,
+  'values': string,
+  'aSymbol': string,
+  'bSymbol': string,
+  'cSymbol': string,
+  'dSymbol': string,
+  'fSymbol': string,
+  'nSymbol': string,
+  'xSymbol': string,
+  'ySymbol': string,
+  'rSymbol': string,
+  'chiSymbol': string
+};
+
+const curveFittingStrings = getStringModule( 'CURVE_FITTING' ) as StringsType;
+
+curveFitting.register( 'curveFittingStrings', curveFittingStrings );
+
+export default curveFittingStrings;
