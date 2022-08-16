a28a6be41a903799bc8817f382527e0d1166d5c5
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:40 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/curveFittingStrings.ts
+++ js/curveFittingStrings.ts
@@ -5,38 +5,66 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import curveFitting from './curveFitting.js';
 
 type StringsType = {
   'adjustableFit': string;
+  'adjustableFitProperty': TReadOnlyProperty<string>;
   'bestFit': string;
+  'bestFitProperty': TReadOnlyProperty<string>;
   'cubic': string;
+  'cubicProperty': TReadOnlyProperty<string>;
   'curve': string;
+  'curveProperty': TReadOnlyProperty<string>;
   'curve-fitting': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'deviations': string;
+  'deviationsProperty': TReadOnlyProperty<string>;
   'equation': string;
+  'equationProperty': TReadOnlyProperty<string>;
   'linear': string;
+  'linearProperty': TReadOnlyProperty<string>;
   'undefined': string;
+  'undefinedProperty': TReadOnlyProperty<string>;
   'fEqualsNumberOfParametersPattern': string;
+  'fEqualsNumberOfParametersPatternProperty': TReadOnlyProperty<string>;
   'nEqualsNumberOfDataPointsPattern': string;
+  'nEqualsNumberOfDataPointsPatternProperty': TReadOnlyProperty<string>;
   'deltaEqualsPattern': string;
+  'deltaEqualsPatternProperty': TReadOnlyProperty<string>;
   'pointCoordinatesPattern': string;
+  'pointCoordinatesPatternProperty': TReadOnlyProperty<string>;
   'theReducedChiSquaredStatisticIs': string;
+  'theReducedChiSquaredStatisticIsProperty': TReadOnlyProperty<string>;
   'quadratic': string;
+  'quadraticProperty': TReadOnlyProperty<string>;
   'residuals': string;
+  'residualsProperty': TReadOnlyProperty<string>;
   'values': string;
+  'valuesProperty': TReadOnlyProperty<string>;
   'aSymbol': string;
+  'aSymbolProperty': TReadOnlyProperty<string>;
   'bSymbol': string;
+  'bSymbolProperty': TReadOnlyProperty<string>;
   'cSymbol': string;
+  'cSymbolProperty': TReadOnlyProperty<string>;
   'dSymbol': string;
+  'dSymbolProperty': TReadOnlyProperty<string>;
   'fSymbol': string;
+  'fSymbolProperty': TReadOnlyProperty<string>;
   'nSymbol': string;
+  'nSymbolProperty': TReadOnlyProperty<string>;
   'xSymbol': string;
+  'xSymbolProperty': TReadOnlyProperty<string>;
   'ySymbol': string;
+  'ySymbolProperty': TReadOnlyProperty<string>;
   'rSymbol': string;
+  'rSymbolProperty': TReadOnlyProperty<string>;
   'chiSymbol': string;
+  'chiSymbolProperty': TReadOnlyProperty<string>;
 };
 
 const curveFittingStrings = getStringModule( 'CURVE_FITTING' ) as StringsType;
