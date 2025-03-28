5634ba81a60991bc080a65f5e015e9265a4d03bb
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-28 16:33:55 -0600

    Use QueryStringMachineModule, https://github.com/phetsims/query-string-machine/issues/54

--- js/curve-fitting/CurveFittingQueryParameters.js
+++ js/curve-fitting/CurveFittingQueryParameters.js
@@ -6,9 +6,10 @@
  * @author Martin Veillette (Berea College)
  */
 
+import { QueryStringMachine } from '../../../query-string-machine/js/QueryStringMachineModule.js';
 import curveFitting from '../curveFitting.js';
 
-const CurveFittingQueryParameters = window.QueryStringMachine.getAll( {
+const CurveFittingQueryParameters = QueryStringMachine.getAll( {
 
   // Determines whether dragged points should be placed such that their coordinates are rounded to the nearest 1.
   // For internal use only, not public facing.
