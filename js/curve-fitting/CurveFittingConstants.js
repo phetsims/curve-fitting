969cbc2b15cc4629d5b022e7b7421fcd515ac0f1
phet-dev <phetadmin@gmail.com>
2026-03-21 04:12:02 -0600

    update copyright dates from daily grunt work

--- js/curve-fitting/CurveFittingConstants.js
+++ js/curve-fitting/CurveFittingConstants.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2020, University of Colorado Boulder
+// Copyright 2015-2026, University of Colorado Boulder
 
 /**
  * Constants used in multiple locations within the 'Curve Fitting' simulation.
--- js/curve-fitting/model/CurveFittingModel.js
+++ js/curve-fitting/model/CurveFittingModel.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2023, University of Colorado Boulder
+// Copyright 2015-2026, University of Colorado Boulder
 
 /**
  * Model container for 'Curve Fitting' simulation.
